52fdc580c1ef89173a6bd22fc6b5e3ae9932fc21
amarieioana <amarieioanaroxana@yahoo.com>
2019-04-10 10:24:33 +0300

    first commit

---
+++ back-end/src/main/java/degree/SmartBuildings/SmartBuildingsApplication.java
@@ -0,0 +1,13 @@
+package degree.SmartBuildings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartBuildingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartBuildingsApplication.class, args);
+	}
+
+}
